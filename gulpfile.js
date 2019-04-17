c63a3406cc1797e8b633ca00b8720665bddba270
for.mom <mom.wowcomp@outlook.com>
2019-04-17 15:49:59 -0500

    auto

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var gulp = require('gulp'),
 watch = require('gulp-watch'),
 postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer');
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested');
 
 
 gulp.task('default', function() {
@@ -14,7 +16,7 @@ gulp.task('html', function() {
 
 gulp.task('styles', function() {
      return gulp.src('./app/assets/styles/styles.css')
-     .pipe(postcss([autoprefixer]))
+     .pipe(postcss([cssvars, nested, autoprefixer]))
      .pipe(gulp.dest('./app/temp/styles'));
 });
 
