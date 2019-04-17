23550bdd130ee0edb9ba8f5bf642ff6b39553d20
for.mom <mom.wowcomp@outlook.com>
2019-04-17 15:32:44 -0500

    commit thru vid 10.5

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch');
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer');
 
 
 gulp.task('default', function() {
@@ -11,7 +13,9 @@ gulp.task('html', function() {
 });
 
 gulp.task('styles', function() {
-     console.log("Imagine Sass or PostCSS tasks running here.");
+     return gulp.src('./app/assets/styles/styles.css')
+     .pipe(postcss([autoprefixer]))
+     .pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', function() {
